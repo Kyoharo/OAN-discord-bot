64dd153d122dc4890878bf26bfe3b4b4226ceda7
Kyo <Kyoharo.abdoataa39@gmail.com>
2023-06-18 16:17:41 +0300

    add command voice

--- main.py
+++ main.py
@@ -35,9 +35,6 @@ class aclient(commands.Bot):
 client = aclient()
 
 
-
-
-
 #---------------------------------------------          ask       -----------------------------------------------------------------
 @client.tree.command(name='ask', description='Ask me a question.')
 async def ask_question(interaction: discord.Interaction, your_question: str):
@@ -208,7 +205,7 @@ async def on_message(message):
 
                     response = await asyncio.to_thread(chat.start, int(user_id), pass_question, language)
                     #convert the response to speech
-                    if "en" in language or "ja" in language:
+                    if "en" in language or "ja" in language and len(response) < 2000:
                         output_path = f"audio_files/{user_id}/output.wav"
                         tts_audio(response, output_path, language)
                         print("tts done")
@@ -274,7 +271,7 @@ async def ask_question(ctx):
                 response = await asyncio.to_thread(chat.start, int(user_id), pass_question,language)
 
                 #convert the response to speech
-                if "en" in language or "ja" in language:
+                if "en" in language or "ja" in language and len(response) < 2000:
                     output_path = f"audio_files/{user_id}/output.wav"
                     tts_audio(response, output_path, language)
                     print("tts done")
@@ -316,6 +313,85 @@ async def ask_question(ctx):
                     print(f"!ask** Guild: {guild.name},   username: {user_name}")
                 except Exception as e:
                     print(f"!ask** username: {user_name}")
+
+
+#---------------------------------------------------------------------------------------------------------------------- voice ----------------------------------------------------------------------
+@client.tree.command(name='voice', description='Communicate with the bot using Record')
+async def ask_question(interaction: discord.Interaction, voice_record: discord.Attachment):
+    user_name = interaction.user.name
+    user_id = str(interaction.user.id)
+    await interaction.response.defer(thinking=True)
+
+    DOWNLOAD_DIR = os.path.join("audio_files", user_id)
+    if not os.path.exists(DOWNLOAD_DIR):
+        os.makedirs(DOWNLOAD_DIR)
+    filename = os.path.join(DOWNLOAD_DIR, voice_record.filename)
+    await voice_record.save(filename)
+
+    if not voice_record.content_type.startswith('audio'):
+        embed = discord.Embed(
+            title="Invalid File",
+            description="Please send a voice record.",
+            color=discord.Color.red()
+        )
+        embed.set_footer(text=f'{user_name}', icon_url=interaction.user.avatar.url)
+        await interaction.followup.send(embed=embed)
+        return
+
+    your_question, language = transcribe_audio(filename)
+    print(your_question, language)
+    pass_question = f"**{user_name}**: {your_question}"
+    response = await asyncio.to_thread(chat.start, int(user_id), pass_question, language)
+
+    if "en" in language or "ja" in language and len(response) < 2000:
+        output_path = f"audio_files/{user_id}/output.wav"
+        tts_audio(response, output_path, language)
+        print("tts done")
+
+        if os.path.exists(output_path):
+            embed = discord.Embed(
+                title=f""">   ``{your_question}``""",
+                description=f"{response}",
+                color=discord.Color.dark_gold()
+            )
+            embed.set_author(
+                name="OAN",
+                icon_url="https://cdn.discordapp.com/attachments/1085541383563124858/1113276038634541156/neka_xp2.png"
+            )
+            embed.set_footer(text=f'{user_name}', icon_url=interaction.user.avatar.url)
+            await interaction.followup.send(embed=embed, file=discord.File(output_path, filename="output.wav"))
+            return
+
+    embeds = []
+    if len(response) > 2000:
+        texts = [response[i:i + 2000] for i in range(0, len(response), 2000)]
+        for text in texts:
+            your_question = your_question[:230]
+            embed = discord.Embed(
+                title=f""">   ``{your_question}``""",
+                description=f"{text}",
+                color=discord.Color.dark_gold()
+            )
+            embed.set_author(
+                name="OAN",
+                icon_url="https://cdn.discordapp.com/attachments/1085541383563124858/1113276038634541156/neka_xp2.png"
+            )
+            embeds.append(embed)
+        view = PageView(embeds)
+        await interaction.followup.send(embed=view.initial(), view=view)
+    else:
+        your_question = your_question[:230]
+        embed = discord.Embed(
+            title=f""">   ``{your_question}``""",
+            description=f"{response}",
+            color=discord.Color.dark_gold()
+        )
+        embed.set_author(
+            name="OAN",
+            icon_url="https://cdn.discordapp.com/attachments/1085541383563124858/1113276038634541156/neka_xp2.png"
+        )
+        embed.set_footer(text=f'{user_name}', icon_url=interaction.user.avatar.url)
+        await interaction.followup.send(embed=embed)
 
 @client.remove_command("help")
 #---------------------------------------------         error CommandNotFound   -----------------------------------------------------------------
